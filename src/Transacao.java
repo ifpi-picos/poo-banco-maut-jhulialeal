2bd3f913941ff8bcdb29e73ff475f804eb814b00
jhulialeal <lealjhu@gmail.com>
2023-10-10 00:40:17 -0300

    correcoes no extrato com historico de transacoes

---
+++ src/Transacao.java
@@ -0,0 +1,9 @@
+public class Transacao {
+    public String tipo;
+    public double valor;
+
+    public Transacao(String tipo, double valor) {
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+}
